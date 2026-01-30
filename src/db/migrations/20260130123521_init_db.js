b8325c72fa76d0245d923484e16dcc14d977a23a
Abdalla Nasser <abdallanaser1432000@gmail.com>
2026-01-30 14:51:30 +0200

    chore: setup postgres connection and migrations

---
+++ src/db/migrations/20260130123521_init_db.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("app_meta", (table) => {
+    table.increments("id").primary();
+    table.string("key").notNullable().unique();
+    table.string("value").notNullable();
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable("app_meta");
+};
